6f7089e03329db3d39ac390e3e1433d7a681b5f2
alsgud <ys26051@naver.com>
2023-03-14 10:24:50 +0900

    초기 커밋.

---
+++ report2/report2.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+double factorial_iter(int n)
+{
+    double result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+double factorial_rec(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial_rec(n - 1);
+    }
+}
+
+int main()
+{
+    int n = 20;
+    double result_iter = factorial_iter(n);
+    double result_rec = factorial_rec(n);
+
+    printf("20! = %f\n", result_iter);
+    printf("20! = %f\n", result_rec);
+
+    return 0;
+}
